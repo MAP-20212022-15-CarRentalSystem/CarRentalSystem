f7b0cb8561cca7433364a1eb4eb5cc4506a3fd06
Shafi Ahmed <ahmed.shafi@graduate.utm.my>
2022-06-06 14:59:48 +0600

    Created Forgot Password Page

--- lib/screens/forgot_password_page.dart
+++ lib/screens/forgot_password_page.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:toast/toast.dart';
 import 'package:vehicle_sharing_app/screens/login_page.dart';
-
 import '../widgets/custom_text_field.dart';
 import '../widgets/widgets.dart';
 
