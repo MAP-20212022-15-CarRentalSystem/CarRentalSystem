0a38033e37ba7f2bbfbbeab79eec0483642c764a
Yusuf <mdyusuf@graduate.utm.my>
2022-06-23 18:29:39 +0800

    Validation for Signup feature

---
+++ lib/services/authentication_service.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:vehicle_sharing_app/screens/login_page.dart';
+
+class AuthenticationService {
+  final FirebaseAuth _firebaseAuth;
+
+  AuthenticationService(this._firebaseAuth);
+
+  Stream<User> get authStateChanges => _firebaseAuth.idTokenChanges();
+
+  /// There are a lot of different ways on how you can do exception handling.
+  /// This is to make it as easy as possible but a better way would be to
+  /// use your own custom class that would take the exception and return better
+  /// error messages. That way you can throw, return or whatever you prefer with that instead.
+  Future<String> signUp({String email, String password}) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return "Signed up";
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+}
