bfd7087481faebd17c180676740484f5387e6630
Yusuf <mdyusuf@graduate.utm.my>
2022-07-07 23:38:46 +0800

    Authentication notifier for registration

--- lib/notifier/auth_notifier.dart
+++ lib/notifier/auth_notifier.dart
@@ -1,25 +1,23 @@
+import 'package:car_rental_system/services/api_state.dart';
+import 'package:car_rental_system/services/network_exceptions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AuthNotifier extends ChangeNotifier {
-  FirebaseAuth _user;
-
-  FirebaseAuth get user {
-    return _user;
-  }
-
-  void setUser(FirebaseAuth user) {
-    _user = user;
-    notifyListeners();
-  }
-
-  //Test.
-  User _userDetails;
-
-  User get userDetails => _userDetails;
-
-  setUserDetails(User user) {
-    _userDetails = user;
-    notifyListeners();
+class SignUpNotifier extends StateNotifier<ApiState<UserCredential>> {
+  SignUpNotifier() : super(const ApiState.initial());
+  Future<void> signUp(String email, String password) async {
+    state = const ApiState.loading();
+    try {
+      final UserCredential data =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      EasyLoading.showSuccess('Account Created Succesfully');
+      state = ApiState.loaded(data: data);
+    } catch (e) {
+      state = ApiState.error(error: NetworkExceptions.getErrorMsg(e));
+    }
   }
 }
