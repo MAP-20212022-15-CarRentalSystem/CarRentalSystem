a73b853ac7d79c05fe617453bd34bd2f5aea4a1d
Yusuf <mdyusuf@graduate.utm.my>
2022-07-07 23:43:59 +0800

    Main functionality when the app runs and initialize firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,70 @@
-import 'dart:io' show Platform;
-
+import 'package:car_rental_system/firebase_options.dart';
+import 'package:car_rental_system/utils/context_less.dart';
+import 'package:car_rental_system/utils/routes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:vehicle_sharing_app/dataHandler/appdata.dart';
-import 'package:vehicle_sharing_app/screens/home_page.dart';
-import 'package:vehicle_sharing_app/screens/login_page.dart';
-import 'package:vehicle_sharing_app/services/authentication_service.dart';
-import 'package:vehicle_sharing_app/services/context_less.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AuthenticationService>(
-          create: (_) => AuthenticationService(FirebaseAuth.instance),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => AppData(),
-        ),
-        StreamProvider(
-          create: (context) =>
-              context.read<AuthenticationService>().authStateChanges,
-        ),
+    initializeDateFormatting();
+    return MaterialApp(
+      supportedLocales: const [
+        Locale('en'),
       ],
-      child: MaterialApp(
-        title: 'Rental Car',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          fontFamily: 'OpenSans',
-          primaryColor: Color.fromRGBO(0, 0, 0, 1),
-          // 27, 34, 46
-        ),
-        home: AuthenticationWrapper(),
-        navigatorKey: ContextLess.navigatorkey,
+      localizationsDelegates: const [
+        FormBuilderLocalizations.delegate,
+      ],
+      title: 'Rental Car',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'OpenSans',
+        primaryColor: const Color.fromRGBO(0, 0, 0, 1),
       ),
+      initialRoute: Routes.splash,
+      onGenerateRoute: generatedRoutes,
+      navigatorKey: ContextLess.navigatorkey,
+      builder: EasyLoading.init(),
     );
   }
 }
 
-class AuthenticationWrapper extends StatelessWidget {
+class AuthenticationWrapper extends StatefulWidget {
+  @override
+  State<AuthenticationWrapper> createState() => _AuthenticationWrapperState();
+}
+
+class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(milliseconds: 100),
+    ).then((value) {
+      final user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        context.nav.pushNamedAndRemoveUntil(Routes.home, (route) => false);
+      } else {
+        context.nav.pushNamedAndRemoveUntil(Routes.login, (route) => false);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final firebaseUser = context.watch<User>();
-
-    if (firebaseUser != null) {
-      return HomePage();
-    } else {
-      return LoginPage();
-    }
+    return Scaffold(
+      body: Container(),
+    );
   }
 }
